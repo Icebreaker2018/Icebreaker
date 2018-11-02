047423e9cbdaaaec64c25ee94f613ab421a5f6c6
ajith <ajith.n17@iiits.in>
2018-11-02 23:13:44 +0530

    static

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
     path('base_layout/',views.base_layout,name="base_layout"),
-    path('about/',views.about),
+    path('about/',views.about,name="about"),
 ]
