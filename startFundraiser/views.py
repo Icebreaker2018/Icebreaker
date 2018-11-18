2972e0127f1fbabbdab256415527a113c724e067
ajith <ajith.n17@iiits.in>
2018-11-18 18:51:52 +0530

    add like

--- startFundraiser/views.py
+++ startFundraiser/views.py
@@ -237,7 +237,7 @@ def detail(request,campaign_id):
             }
 
 
-    return render(request, 'startFundraiser/detail.html', context)
+    return render(request, 'startFundraiser/detail.html', context,)
 
 
 @login_required(login_url="http://127.0.0.1:8000/register/login/")
