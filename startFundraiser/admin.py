19b65bc0887dd589a316a457e19bad59b13b0f12
ajith <ajith.n17@iiits.in>
2018-11-18 18:25:56 +0530

    added likes

--- startFundraiser/admin.py
+++ startFundraiser/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Campaign, FAQs, Update, Post,comment
+from .models import Campaign, Faqs, Update, Post,comment,Backers
 
 admin.site.register(Campaign)
-admin.site.register(FAQs)
+admin.site.register(Faqs)
 admin.site.register(Update)
 admin.site.register(Post)
 admin.site.register(comment)
+admin.site.register(Backers)
